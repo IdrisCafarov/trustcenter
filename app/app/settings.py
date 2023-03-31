795482bc2cac49e90244cedfb4c7fcea8e3cb081
IdrisCafarov <idris.ceferov244@gmail.com>
2023-03-31 04:30:11 +0400

    changes

--- app/app/settings.py
+++ app/app/settings.py
@@ -150,15 +150,15 @@ REST_FRAMEWORK = {
 
 
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'idris.ceferov244@gmail.com'
 SERVER_EMAIL = 'idris.ceferov244@gmail.com'
-EMAIL_USE_TLS = False
-EMAIL_USE_SSL = True
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'idris.ceferov244@gmail.com'
 EMAIL_HOST_PASSWORD = 'kkbbqkjhhfnhwhvk'
-EMAIL_PORT = 465
+EMAIL_PORT = 587
 
 
 
