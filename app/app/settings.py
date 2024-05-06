23f0d3575d5d81e8d986cdee71dc4f75d10c9726
IdrisCafarov <idris.ceferov244@gmail.com>
2024-05-06 11:30:03 +0800

    changes

--- app/app/settings.py
+++ app/app/settings.py
@@ -151,15 +151,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 CSRF_COOKIE_SECURE = True
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = 'idris.ceferov244@gmail.com'
-SERVER_EMAIL = 'idris.ceferov244@gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'idris.ceferov244@gmail.com'
-EMAIL_HOST_PASSWORD = 'kkbbqkjhhfnhwhvk'
-EMAIL_PORT = 587
 
 
 
